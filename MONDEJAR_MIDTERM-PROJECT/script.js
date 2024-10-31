504be0e94ce8571fab020c63104b833dbf239b9e
Ralph Ryan N. Mondejar <ralphryanmondejar299@gmail.com>
2024-10-31 21:12:07 +0800

    Delete MONDEJAR_MIDTERM-PROJECT directory

--- MONDEJAR_MIDTERM-PROJECT/script.js
+++
@@ -1,6 +0,0 @@
-
-// Simple JS function to show alert on form submission
-document.querySelector('form[name="submit-to-google-sheet"]').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert("Thank you! Your message has been submitted successfully.");
-});
